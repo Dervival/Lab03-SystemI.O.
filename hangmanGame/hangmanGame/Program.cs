b218bf79beeaf15cb4c5af7cc2439a77cc7c61f0
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-10 09:39:16 -0800

    Initialization of files

---
+++ hangmanGame/hangmanGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace hangmanGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int Test()
+        {
+            return 1;
+        }
+    }
+}
---
+++ hangmanGame/hangmanGameUnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using hangmanGame;
+
+namespace hangmanGameUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrange
+            //act
+            //assert
+            Assert.Equal(1, Program.Test());
+        }
+    }
+}
